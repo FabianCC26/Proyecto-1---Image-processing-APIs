63b7f4ca790f0cb4f8c5d4a9714f72a35c377ffd
FabianCC26 <fabian.castillo26@outlook.com>
2022-09-29 18:06:32 -0600

    Image procesing edits

--- Image procesing/main.cpp
+++ Image procesing/main.cpp
@@ -3,7 +3,12 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core.hpp>
 
+#include <iostream>
+using namespace std;
 
+
+
+//gamma correction operations 
 
 void gammaCorrection(const cv::Mat &src, cv::Mat &dst, const float gamma)
 {
@@ -18,29 +23,158 @@ void gammaCorrection(const cv::Mat &src, cv::Mat &dst, const float gamma)
     LUT(src, table, dst);
 }
 
+//Grey filter operations
 
-int main(){
-    cv::Mat image;
-    image = cv::imread("./sample.jpeg", cv::IMREAD_COLOR);
+
+void GreyFilter(cv::Mat image){
+
+    cv::Mat greyImg;
+
+    cv:: cvtColor(image,greyImg, cv::COLOR_RGB2GRAY);
+
+    cv::imshow("Grey Filter", greyImg);
+
+}
+
+//gaussian blur filter operations
+
+void GaussianFilter (cv::Mat image, int blurlvl){
 
     cv::Mat gaussianImg;
-    cv::Mat grayImg;
+
+    cv:: GaussianBlur(image,gaussianImg,cv::Size(blurlvl,blurlvl),0);
+
+    cv::imshow("Gaussian", gaussianImg);
+
+
+}
+
+//Bright filter operations
+
+void BrightFilter (cv::Mat image, int brillolvl){
+
     cv::Mat BrightImg;
+
+    image.convertTo(BrightImg,-1,2,brillolvl);
+
+    cv::imshow("Bright", BrightImg);
+
+}
+
+//Gamma filter operations 
+
+void GammaFilter (cv::Mat image, int gammalvl){
+
     cv::Mat GammaImg;
 
-    cv:: GaussianBlur(image,gaussianImg,cv::Size(45,45),0);
-    cv:: cvtColor(image,grayImg, cv::COLOR_RGB2GRAY);
+    gammaCorrection(image, GammaImg, gammalvl);
 
-    //last 2 spaces for change brightness
-    image.convertTo(BrightImg,-1,2,40);
 
-    gammaCorrection(image, GammaImg, 2.2);
-
-    cv::imshow("Original image", image);
     cv::imshow("Gamma", GammaImg);
-    cv::imshow("Gaussian", gaussianImg);
-    cv::imshow("Gray", grayImg);
-    cv::imshow("Bright", BrightImg);
+
+}
+
+//main function
+
+int main(){
+
+    //Image extraction
+
+    cv::Mat image;
+
+    image = cv::imread("./sample.jpeg", cv::IMREAD_COLOR);
+
+    int filter;
+
+    //user choose filter 
+
+    cout<< "Digite el filtro deseado \n  1.Filtro EScala de grises \n  2.Filtro Gaussian Blur \n  3.Correccion de Brillo \n  4.Correccion de Gamma \n  "; cin>>filter;
+
+
+    if (filter == 1){
+
+        GreyFilter(image);
+
+    }
+
+
+    if (filter == 2){
+
+        int blurlvl;
+
+        //user choose filter parameters
+
+        cout<< "Digite el nivel deseado de blur ( 5, 15, 25, 35, 45):  "; cin>>blurlvl;
+
+        //filter limitations
+
+        if (blurlvl == 5 || blurlvl == 15  || blurlvl == 25  || blurlvl == 35  || blurlvl == 45 ) {
+
+            GaussianFilter(image,blurlvl);
+        }
+        else
+        {
+            main();
+
+        }
+
+        
+
+    }
+
+    if (filter == 3){
+
+        int brillolvl;
+
+        //user choose filter parameters
+
+        cout<< "Digite el nivel deseado de brillo (-100 -> 100):  "; cin>>brillolvl;
+
+        //filter limitations
+
+        if (brillolvl > -101 && brillolvl < 101){
+
+            BrightFilter(image,brillolvl);
+        }
+        else
+        {
+            main();
+        }
+
+    }
+
+    if (filter == 4){
+
+        int gammalvl;
+
+        //user choose filter parameters
+
+        cout<< "Digite el nivel deseado de gamma (1 -> 5):  "; cin>>gammalvl;
+
+
+        //filter limitations
+
+
+        if (gammalvl > 0 && gammalvl < 6){
+
+            GammaFilter(image,gammalvl);
+        }
+        else
+        {
+            main();
+        }
+
+    }
+
+    //in case of not choosing a valid filter
+
+    if (filter < 1 || filter > 4) {
+
+        main();
+    }
+
+
     cv::waitKey(0);
+
     return 0;
 }
